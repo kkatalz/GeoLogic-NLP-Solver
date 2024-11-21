fb116514c38592cf43d68e06b6fc67541dd554bc
kkatalz <zlata.kiev2004@gmail.com>
2024-11-21 22:15:31 +0200

    Refactored SemanticAnalyzer to support multiple task extraction and calculation

--- semantic_analyzer.py
+++ semantic_analyzer.py
@@ -1,38 +1,46 @@
 class SemanticAnalyzer:
+
     def __init__(self, udpipe_result):
         self.result = udpipe_result
 
-    def extract_task(self):
-        if "площа" in self.result:
-            return "area"
-        elif "периметр" in self.result:
-            return "perimeter"
-        elif "кут" in self.result:
-            return "angle"
-        elif "медіана" in self.result:
-            return "median"
-        elif "радіус" in self.result:
-            return "radius"
-        elif "діаметр" in self.result:
-            return "diameter"
-        else:
-            return None
+    def extract_task(self, udpipe_result):
+        find_list = ["периметр", "площа", "кут",
+                     "висота", "бісектриса", "медіана", "сторона"]
+        task = []
+        start_checking = False
 
-    def calculate(self, task, elements):
-        # make 6.928203230275509 to 6.93
-        if task == "area":
-            side = float(elements.get("сторона", 0))
-            result = round((pow(side, 2) * pow(3, 0.5)) / 4, 2)
-            return result
+        for word in udpipe_result:
+            if word == "знайти":
+                start_checking = True  # Start checking words after this
+                continue
+            if start_checking and word in find_list:
+                task.append(word)
 
-        elif task == "perimeter":
-            side = float(elements.get("сторона", 0))
-            return side * 3
+        return task
 
-        elif task == "angle":
-            return 60
+    def calculate(self, tasks, elements):
 
-        elif task == "angles":
-            return 180
+        results = []
+        for task in tasks:
+            if task == "площа":
+                side = float(elements.get("сторона", 0))
+                result = round((pow(side, 2) * pow(3, 0.5)) / 4, 2)
+                results.append(f"{task}: {result}")
 
-        return None
+            elif task == "периметр":
+                side = float(elements.get("сторона", 0))
+                result = round(side * 3, 2)
+                results.append(f"{task}: {result}")
+
+            elif task == "кут":
+                results.append(f"{task}: 60")
+
+            elif task == "angles":
+                results.append(f"{task}: 180")
+
+            elif task in ["висота", "бісектриса", "медіана"]:
+                side = float(elements.get("сторона", 0))
+                result = round((pow(3, 2) * side) / 2, 2)
+                results.append(f"{task}: {result}")
+
+        return results if results else ["No valid tasks found."]
