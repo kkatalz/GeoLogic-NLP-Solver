a1ff47abef75535026d64a478b9732e599a067be
kkatalz <zlata.kiev2004@gmail.com>
2024-11-22 00:00:40 +0200

    Removed connection for number and its measure

--- preprocessor.py
+++ preprocessor.py
@@ -6,9 +6,9 @@ class Preprocessor:
     def normalize_text(raw_text):
 
         raw_text_lower = raw_text.lower()
-        text = raw_text_lower.replace("–", "дорівнює")  # Dash replacement
-        text = text.replace("см", " cm")  # Add space before units
-        text = text.replace(",", ".")  # Replace commas with dots for floats
+        text = raw_text_lower.replace("–", "дорівнює")
+        text = text.replace("см", " cm")
+        text = text.replace(",", ".")
         text = text.replace("обчисліть", "знайдіть")
         return text
 
@@ -22,8 +22,7 @@ class Preprocessor:
         i = 0
         while i < len(tokens):
             token = tokens[i]
-            if token == "дорівнювати":  # Look for the keyword indicating a value
-                # Search for the nearest valid key (geometric property)
+            if token == "дорівнювати":
                 key = None
                 for k in range(i - 1, -1, -1):  # Search backward for a valid key
                     if tokens[k].isalnum():  # Ensure the token is a valid word
@@ -32,11 +31,8 @@ class Preprocessor:
 
                 value_tokens = []
                 # Collect all tokens that form the value (e.g., numbers and symbols like √)
-                for j in range(i + 1, len(tokens)):
-                    if tokens[j].isalnum() or tokens[j] in "√":
-                        value_tokens.append(tokens[j])
-                    else:
-                        break
+                if tokens[i+1].isalnum() or tokens[i+1] in "√":
+                    value_tokens.append(tokens[i+1])
 
                 if key and value_tokens:
                     # Combine tokens into a single value
